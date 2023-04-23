34ec86ff5d386c19cd9a24fc17aab97c45f7824c
Ebiladou <oyinebiladouomemu@gmail.com>
2023-04-23 05:59:29 -0700

    updated 4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Prints the alphabet, without q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (i != 4 && i != 15)
+		{
+			putchar(i + 'a');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
