0a7f8a6c7012b617ae34e3f2fd84b15c7aa51e4f
Ebiladou <oyinebiladouomemu@gmail.com>
2023-04-24 10:23:47 -0700

    updated 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two digits.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (j != 56 || i != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
